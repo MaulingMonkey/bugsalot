01cc5e293d0478f28aceeb94d7a2d0ce09ff8ef9
MaulingMonkey <git@maulingmonkey.com>
2019-06-21 18:05:03 -0700

    scripts\install.cmd:  Skip WSL for CI builds, enable command echo.

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -1,5 +1,7 @@
 @setlocal
+@echo on
 @set ERRORS=0
+@if not defined CI where wsl >NUL 2>NUL && goto :cannot-auto-install-wsl
 @where rustup >NUL 2>NUL && goto :skip-install-rustup
 @where curl >NUL 2>NUL || goto :cannot-auto-install-rustup
 curl -sSf -o %TEMP%\rustup-init.exe https://win.rustup.rs
@@ -17,12 +19,10 @@ if not defined CI rustup target add --toolchain nightly i686-pc-windows-msvc    
 if not defined CI rustup target add --toolchain nightly x86_64-pc-windows-msvc    || set ERRORS=1
                   rustup target add --toolchain nightly wasm32-unknown-unknown    || set ERRORS=1
                   rustup target add --toolchain nightly i686-linux-android        || set ERRORS=1
-
-if defined CI wslconfig /setdefault ubuntu
-wsl curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup-init.sh   || set ERRORS=1
-wsl sh rustup-init.sh --default-toolchain stable -y                               || set ERRORS=1
-wsl rustup toolchain install stable'                                              || set ERRORS=1
-wsl rustup toolchain install nightly'                                             || set ERRORS=1
+if not defined CI wsl curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup-init.sh   || set ERRORS=1
+if not defined CI wsl sh rustup-init.sh --default-toolchain stable -y                               || set ERRORS=1
+if not defined CI wsl rustup toolchain install stable'                                              || set ERRORS=1
+if not defined CI wsl rustup toolchain install nightly'                                             || set ERRORS=1
 
 @echo.
 @echo.
@@ -38,7 +38,21 @@ wsl rustup toolchain install nightly'                                           
 @echo.
 @echo.
 @echo INSTALLATION ERRORS:
-@echo curl not found in %%PATH%%, cannot auto-install rustup.
+@echo curl not found in %%PATH%%, cannot auto-install rustup.  Install manually via:
+@echo     https://rustup.rs/
+@echo and try this script again.
+@echo.
+@echo.
+@endlocal && exit /b 1
+
+:cannot-auto-install-wsl
+@echo.
+@echo.
+@echo INSTALLATION ERRORS:
+@echo wsl not found in %%PATH%%, cannot auto-install Windows Subsystem for Linux.
+@echo Install manually via the instructions at:
+@echo     https://docs.microsoft.com/en-us/windows/wsl/install-win10
+@echo and try this script again.
 @echo.
 @echo.
 @endlocal && exit /b 1
