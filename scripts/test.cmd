acc41640a7c50ef3027ea0f3c99b16d3e0591041
MaulingMonkey <git@maulingmonkey.com>
2019-06-30 17:27:21 -0700

    scripts/test.cmd:  Add android emulator testing support.

--- scripts/test.cmd
+++ scripts/test.cmd
@@ -75,9 +75,9 @@
 
 @if not "%PLATFORM%" == "*" goto :skip-platform-wildcard
   @call :build "%CHANNEL%" "%CONFIG%" windows
-  @call :build "%CHANNEL%" "%CONFIG%" android
   @call :build "%CHANNEL%" "%CONFIG%" linux
   @call :build "%CHANNEL%" "%CONFIG%" wasm
+  @call :build "%CHANNEL%" "%CONFIG%" android
   @endlocal && set ERRORS=%ERRORS%&& exit /b 0
 :skip-platform-wildcard
 
@@ -95,30 +95,66 @@
 
 @if not "%ERRORS%" == "0" goto :build-one-skipped
 @if /I "%CHANNEL%"  == "beta"                echo Skipping %CHANNEL% %CONFIG% %PLATFORM%: Beta toolchain&& goto :build-one-skipped
-@if /I "%PLATFORM%" == "android"             echo Skipping %CHANNEL% %CONFIG% %PLATFORM%: Build not fully configured&& goto :build-one-skipped
 @if /I "%PLATFORM%" == "linux" if defined CI echo Skipping %CHANNEL% %CONFIG% %PLATFORM%: Appveyor doesn't have WSL installed&& goto :build-one-skipped
 
 :: Parameters -> Settings
 
-@set CARGO_FLAGS=--verbose
+@set CARGO_FLAGS= 
 @if /i "%CONFIG%" == "release"   set CARGO_FLAGS=%CARGO_FLAGS% --release
+
+
+
+:: Build
 
 @if /i "%PLATFORM%" == "windows" cargo +%CHANNEL% test --all %CARGO_FLAGS% || goto :build-one-error
 @if /i "%PLATFORM%" == "windows" goto :build-one-successful
 
-@if /i "%PLATFORM%" == "android" rustup toolchain list | findstr default | findstr x86_64 && set "NATIVE_ARCH=x86_64" || set "NATIVE_ARCH=i686"
-@if /i "%PLATFORM%" == "android" cargo +%CHANNEL% test             %CARGO_FLAGS% --target=%NATIVE_ARCH%-linux-android || goto :build-one-error
-@if /i "%PLATFORM%" == "android" cargo +%CHANNEL% build --examples %CARGO_FLAGS% --target=%NATIVE_ARCH%-linux-android || goto :build-one-error
-@if /i "%PLATFORM%" == "android" goto :build-one-successful
 
-@if /i "%PLATFORM%" == "linux" %WINDIR%\System32\bash --login -c 'cargo +%CHANNEL% test             %CARGO_FLAGS%' || goto :build-one-error
-@if /i "%PLATFORM%" == "linux" %WINDIR%\System32\bash --login -c 'cargo +%CHANNEL% build --examples %CARGO_FLAGS%' || goto :build-one-error
+
+@if /i not "%PLATFORM%" == "android" goto :skip-platform-android
+@set ANDROID_AVD_NAME=Nexus_5X_API_29_x86
+@set JAVA_HOME=%ProgramFiles%\Android\Android Studio\jre\
+@set PATH=%LOCALAPPDATA%\Android\Sdk\platform-tools\;%PATH%
+@set PATH=%LOCALAPPDATA%\Android\Sdk\ndk-bundle\toolchains\llvm\prebuilt\windows-x86_64\bin;%PATH%
+adb start-server
+start "" /B "%LOCALAPPDATA%\Android\Sdk\emulator\emulator" -no-audio -no-window -no-snapshot -no-boot-anim @%ANDROID_AVD_NAME%
+:: -no-snapshot:  loading one would speed things up, but this can cause problems in terminated emulators.  Saving is undesired too.
+adb wait-for-local-device
+@for /f "tokens=1" %%d in ('adb devices ^| findstr emulator-') do @set EMULATOR_DEVICE=%%d
+@adb -s %EMULATOR_DEVICE% shell getprop ro.product.cpu.abi | findstr x86_64 && set "NATIVE_ARCH=x86_64" || set "NATIVE_ARCH=i686"
+@pushd "%~dp0windows-android"
+@set ANDROID_ERROR=0
+cargo +%CHANNEL% build --all --tests %CARGO_FLAGS% --target=%NATIVE_ARCH%-linux-android || set ANDROID_ERROR=1
+@popd
+@if not "%ANDROID_ERROR%" == "0" goto :build-one-error
+@pushd "%~dp0..\target\%NATIVE_ARCH%-linux-android\%CONFIG%"
+@set ANDROID_TEST_NAME=
+@for /f "" %%n in ('dir /OD /B bugsalot-* ^| findstr /v \.d') do @set "ANDROID_TEST_NAME=%%n"
+@if not defined ANDROID_TEST_NAME set ANDROID_TEST_NAME=could_not_find_android_test
+adb -s %EMULATOR_DEVICE% push "%ANDROID_TEST_NAME%" /data/local/tmp/bugsalot_unit_tests || set ANDROID_ERROR=1
+@popd
+adb -s %EMULATOR_DEVICE% shell chmod 755 /data/local/tmp/bugsalot_unit_tests || set ANDROID_ERROR=1
+adb -s %EMULATOR_DEVICE% shell /data/local/tmp/bugsalot_unit_tests || set ANDROID_ERROR=1
+@taskkill /FI "WINDOWTITLE eq Android Emulator - %ANDROID_AVD_NAME%:*" 2>NUL
+::@taskkill /F /IM "emulator.exe"
+@if not "%ANDROID_ERROR%" == "0" goto :build-one-error
+@goto :build-one-successful
+:skip-platform-android
+
+
+
+@if /i "%PLATFORM%" == "linux" "%WINDIR%\System32\bash" --login -c 'cargo +%CHANNEL% test             %CARGO_FLAGS%' || goto :build-one-error
+@if /i "%PLATFORM%" == "linux" "%WINDIR%\System32\bash" --login -c 'cargo +%CHANNEL% build --examples %CARGO_FLAGS%' || goto :build-one-error
 @if /i "%PLATFORM%" == "linux" goto :build-one-successful
+
+
 
 @if /i "%PLATFORM%" == "wasm" call :install-cargo-web                                                  || goto :build-one-error
 @if /i "%PLATFORM%" == "wasm" call :add-chrome-to-path                                                 || goto :build-one-error
 @if /i "%PLATFORM%" == "wasm" cargo +%CHANNEL% web build --target=wasm32-unknown-unknown %CARGO_FLAGS% || goto :build-one-error
 @if /i "%PLATFORM%" == "wasm" goto :build-one-successful
+
+
 
 @echo Unrecognized %%PLATFORM%%: %PLATFORM%
 @goto :build-one-error
